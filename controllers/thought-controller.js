3b443d93aaae2a7ab967b9c57b65ca483da6b6dc
Padredilg <padredilg@knights.ucf.edu>
2021-10-19 11:19:12 -0400

    built thought controller methods

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,67 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
+    // GET all thoughts
+    getAllThought(req, res){
+        Thought.find({})//finds all thoughts
+            .then(thoughtsData => res.json(thoughtsData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
 
+    },
+
+    // GET single thought by /:id
+    getThoughtById({ params }, res){
+        Thought.findOne({ _id: params.id })//finds the thought
+            //not sure if should populate thoughts as well
+            .then(thoughtData => {
+                if(!thoughtData){
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;  
+                }
+                res.json(thoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    
+    // POST new thought
+    createThought({ body }, res){
+        Thought.create(body)
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => res.status(400).json(err))
+    },
+    
+    // PUT update thought by /:id
+    updateThought({ params, body }, res){
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(thoughtData => {
+            if (!thoughtData) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    
+    // DELETE thought by /:id
+    deleteThought({ params }, res){
+        Thought.findOneAndDelete({ _id: params.id })
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(thoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 }
+
 //will be required by routes
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,7 +9,15 @@ const {
 
 router.route('/')
     .get(getAllThought)// Get all thoughts
-    .post(createThought);
+    .post(createThought);//create new thought
+
+// push created thought _id to associated user thoughts array
+// example data:
+// {
+//   "thoughtText": "Here's a cool thought...",
+//   "username": "lernantino",
+//   "userId": "5edff358a0fcb779aa7b118b"
+// } meaning req can be destructured to { body }
 
 router.route('/:id')
     .get(getThoughtById)// Get thoughts by ID
@@ -17,13 +25,5 @@ router.route('/:id')
     .delete(deleteThought);// Delete thought by ID
 
 
-
-// Post create new thought - push created thought _id to associated user thoughts array
-// example:
-// {
-//   "thoughtText": "Here's a cool thought...",
-//   "username": "lernantino",
-//   "userId": "5edff358a0fcb779aa7b118b"
-// } meaning req can be destructured to { body }
 
 module.exports = router;
