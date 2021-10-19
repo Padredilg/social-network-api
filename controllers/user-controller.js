8bf2af7c48e97b878e5c6553ca0b7233fe80c336
Padredilg <padredilg@knights.ucf.edu>
2021-10-19 09:53:02 -0400

    built create and update user methods for user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -38,14 +38,28 @@ const userController = {
     },
 
     // post new user
-    // createUser
+    createUser({ body }, res){
+        User.create(body)
+            .then(userData => res.json(userData))
+            .catch(err => res.status(400).json(err))
+    },
 
     // update user by Id
-    // updateUser
+    updateUser({ params, body }, res){
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
 
     // delete user by id
     // deleteUser
 }
 
-//This will be required (possibly with destructuring method) by routes
+//This will be required by routes
 module.exports = userController;
